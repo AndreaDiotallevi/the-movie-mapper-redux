8fd73511b25142df1154dd28e2a633160b9fdbbf
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-04-27 16:33:05 +0100

    Extract MovieDetail component from MovieList component

---
+++ src/components/MovieDetail.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+import noPhotoAvailable from "../assets/no-photo-available.jpg";
+
+const MovieDetail = ({ imdbID, title, plot, posterURL, releaseDate }) => {
+  const handleImageUrlError = (event) => {
+    event.target.src = noPhotoAvailable;
+  };
+
+  return (
+    <li className="movie" data-test={`movie-${imdbID}`} key={imdbID}>
+      <div className="movie-image">
+        <a
+          href={`https://www.imdb.com/title/${imdbID}/`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="movie-poster"
+            data-test={`movie-poster-url-${imdbID}`}
+            alt={`movie-poster-url-${imdbID}`}
+            src={`${posterURL}`}
+            onError={handleImageUrlError}
+          ></img>
+        </a>
+      </div>
+      <div className="movie-info">
+        <div className="title-plot-container">
+          <div className="movie-title-container">
+            <a
+              className="movie-title-anchor"
+              href={`https://www.imdb.com/title/${imdbID}/`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className="movie-title" data-test={`movie-title-${imdbID}`}>
+                {title.toUpperCase()}
+              </h2>
+            </a>
+          </div>
+          <p className="movie-plot" data-test={`movie-plot-${imdbID}`}>
+            {plot}
+          </p>
+        </div>
+        <div className="date-rating-container">
+          <p
+            className="movie-release-date"
+            data-test={`movie-release-date-${imdbID}`}
+          >
+            <span className="movie-release-date-title"></span>
+            {releaseDate}
+          </p>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default MovieDetail;
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchMoviesFromCountry } from "../actions";
+import MovieDetail from "./MovieDetail";
 import queryString from "query-string";
-
-import noPhotoAvailable from "../assets/no-photo-available.jpg";
 
 class MovieList extends React.Component {
   values = queryString.parse(this.props.location.search);
@@ -27,71 +26,19 @@ class MovieList extends React.Component {
     }
   };
 
-  handleImageUrlError = (event) => {
-    event.target.src = noPhotoAvailable;
-  };
-
   render() {
     return (
       <div className="movie-list-component">
         <div className="movie-list-container">
           <ul>
-            {this.filterMovies().map((movie, index) => (
-              <li
-                className="movie"
-                data-test={`movie-${movie.imdbID}`}
-                key={index}
-              >
-                <div className="movie-image">
-                  <a
-                    href={`https://www.imdb.com/title/${movie.imdbID}/`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <img
-                      className="movie-poster"
-                      data-test={`movie-poster-url-${movie.imdbID}`}
-                      alt={`movie-poster-url-${movie.imdbID}`}
-                      src={`${movie.Poster}`}
-                      onError={this.handleImageUrlError}
-                    ></img>
-                  </a>
-                </div>
-                <div className="movie-info">
-                  <div className="title-plot-container">
-                    <div className="movie-title-container">
-                      <a
-                        className="movie-title-anchor"
-                        href={`https://www.imdb.com/title/${movie.imdbID}/`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <h2
-                          className="movie-title"
-                          data-test={`movie-title-${movie.imdbID}`}
-                        >
-                          {movie.Title.toUpperCase()}
-                        </h2>
-                      </a>
-                    </div>
-                    <p
-                      className="movie-plot"
-                      data-test={`movie-plot-${movie.imdbID}`}
-                    >
-                      {movie.Plot}
-                    </p>
-                  </div>
-                  <div className="date-rating-container">
-                    <p
-                      className="movie-release-date"
-                      data-test={`movie-release-date-${movie.imdbID}`}
-                    >
-                      <span className="movie-release-date-title"></span>
-                      {movie.Released}
-                    </p>
-                  </div>
-                </div>
-              </li>
+            {this.filterMovies().map((movie) => (
+              <MovieDetail
+                imdbID={movie.imdbID}
+                title={movie.Title}
+                plot={movie.Plot}
+                posterURL={movie.Poster}
+                releaseDate={movie.Released}
+              />
             ))}
           </ul>
         </div>
