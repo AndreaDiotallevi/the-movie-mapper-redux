08b2bbef6e99ef9d68024f68d977adc0e35d62c9
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-02 10:12:21 +0100

    Add test if country code not fetched

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,21 +1,26 @@
 import moxios from "moxios";
 
 import { storeFactory } from "../../test/testUtils";
-import { fetchCountryCode } from "./";
+import { fetchCoordinates, fetchCountryCode } from "./";
+
+beforeEach(() => {
+  moxios.install();
+});
+
+afterEach(() => {
+  moxios.uninstall();
+});
+
+// describe("fetchCoordinates", () => {
+//   test('adds coordinates to state from user click on map', () => {
+
+//   })
+// });
 
 describe("fetchCountryCode action creator", () => {
-  beforeEach(() => {
-    moxios.install();
-  });
-
-  afterEach(() => {
-    moxios.uninstall();
-  });
-
-  test("adds country code to state", async () => {
-    const coordinates = [44.59117130368516, 10.65088757396451];
+  test("sets country code to state if clicked on a country", async () => {
+    const coordinates = [44.59117130368516, 10.65088757396451]; // Italy
     const store = storeFactory();
-    console.log(store.getState());
 
     moxios.wait(() => {
       const request = moxios.requests.mostRecent();
@@ -28,5 +33,22 @@ describe("fetchCountryCode action creator", () => {
     await store.dispatch(fetchCountryCode(coordinates));
     const newState = store.getState();
     expect(newState.countryCode).toBe("it");
+  });
+
+  test("sets infoWindowOn to true if clicked on the sea", async () => {
+    const coordinates = [44.59117130368516, -21.301775147928982]; // Sea
+    const store = storeFactory();
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: { error: "Unable to geocode" },
+      });
+    });
+
+    await store.dispatch(fetchCountryCode(coordinates));
+    const newState = store.getState();
+    expect(newState.infoWindowOn).toBe(true);
   });
 });
